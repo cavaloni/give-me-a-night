9d81692ec8b178fbc6672a320009c1fe44e02de5
cavaloni <chad.avalon1@gmail.com>
2017-02-10 23:41:03 -0800

    server.js update

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const app = express();
 
 app.use('/', express.static(__dirname + '/build'));
 
+app.get('/', function(request, response) {
+  response.render(__dirname + '/build');
+});
+
 app.listen(process.env.PORT || 8081, () => {console.log(`Your app is listening on port 8081`);})
