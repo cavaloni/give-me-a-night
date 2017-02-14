11444615ff0ab33402ca78da79abd0b3b46c9b3e
cavaloni <chad.avalon1@gmail.com>
2017-02-14 11:25:56 -0800

    heroku build update

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ const VENDOR_LIBS = [
 ];
 
 module.exports = {
+  context,
   entry:  {
     bundle: [
       'babel-polyfill',
@@ -33,7 +34,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'index.html')
+      template: path.resolve(__dirname, './index.html')
     }),
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor', 'manifest']
