daa06eb3b741e7270e284faddd13f7a6c70a8d7a
cavaloni <chad.avalon1@gmail.com>
2017-02-16 15:55:07 -0800

    heroku build update

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -30,7 +30,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build/js'),
     filename: '[name].[chunkhash].js',
-    publicPath: '/js/'
+    publicPath: '/'
   },
   plugins: [
     new HtmlWebpackPlugin(
